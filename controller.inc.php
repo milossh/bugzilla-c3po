e5d2303d24f7eea5e819670f2c992da1d06c5c65
pascalc <pascal.chevrel@free.fr>
2012-10-31 04:22:10 +0100

    update script to use www.mozilla.org component and automatically select locale

--- controller.inc.php
+++ controller.inc.php
@@ -5,7 +5,9 @@ include_once 'langconfig.php';
 // Without this one the script ain't going to work
 // we fetch config.cgi based on this one
 // also, we use it to create bugs on referring installation
-$bugzilla_url = 'https://landfill.bugzilla.org/bugzilla-3.6-branch/';
+//~ $bugzilla_url = 'https://landfill.bugzilla.org/bugzilla-3.6-branch/';
+//~ $bugzilla_url = 'https://landfill.bugzilla.org/bugzilla-4.0-branch/';
+$bugzilla_url = 'https://bugzilla.mozilla.org/';
 
 // This function explodes a string for a given separator
 // and removes whitespaces from created array elements
@@ -13,5 +15,3 @@ function clean_explode($input, $separator) {
     $input = trim(preg_replace('|\\s*(?:' . preg_quote($separator) . ')\\s*|', $separator, $input));
     return explode($separator, $input);
 }
-
-?>
